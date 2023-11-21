eb15510acbf77843eee2bc5220586a273ced72af
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-11-21 00:18:36 -0500

    metodo para redireccionar con multiples paginas en navegador

---
+++ src/app/(marketing)/shop/[...id]/page.jsx
@@ -0,0 +1,13 @@
+import TitlePage from '@/components/TitlePage'
+import React from 'react'
+
+const Id = ({params}) => {
+  return (
+    <>
+    <TitlePage text="Id"/>
+    <p className='text-center text-black'></p>
+    </>
+  )
+}
+
+export default Id
---
+++ src/app/(marketing)/shop/page.jsx
@@ -0,0 +1,10 @@
+import TitlePage from '@/components/TitlePage'
+import React from 'react'
+
+const Shop = ({params}) => {
+  return (
+<TitlePage text="Shop"/>  
+)
+}
+
+export default Shop
