bd5fccbf807a3153888a03e5f4f1e4b34ad2790c
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-11-21 00:08:21 -0500

    tomado parametros para direccion de rutas

---
+++ src/app/(marketing)/blog/[info]/page.jsx
@@ -0,0 +1,13 @@
+import TitlePage from '@/components/TitlePage'
+import React from 'react'
+
+const Info = ({params}) => {
+  return (
+    <>
+    <TitlePage text="Info"/>
+    <p className='text-center text-black'>{params.info}</p>
+    </>
+  )
+}
+
+export default Info
---
+++ src/app/(marketing)/blog/page.jsx
@@ -0,0 +1,10 @@
+import TitlePage from '@/components/TitlePage'
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <TitlePage text="Blog"/>
+  )
+}
+
+export default Blog
